51a37bba681afb3bfb9eaf57606dc258c8b463a0
Scott <hello@scottcrowningshield.com>
2018-06-08 16:14:53 +0200

    Remove Font Awesome 4.7.0

---
+++ app/mustard-cutter.js
@@ -0,0 +1,8 @@
+if ("querySelector" in document && "localStorage" in window && "addEventListener" in window) {
+    // load the javascript
+}
+
+
+if ("Promise" in window && "forEach" in Array.prototype) {
+
+}
